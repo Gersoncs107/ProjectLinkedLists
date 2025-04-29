0886256fb2e8f5ca5b937047cdc1bc483a1e6186
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-28 23:30:16 -0300

    gitAdd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // index.js
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import raidenImage from "./raiden.jpg";
 
-document.body.innerHTML = `<h1>${greeting}</h1>`;
+document.body.innerHTML = `
+  <h1>${greeting}</h1>
+  <img src="${raidenImage}" alt="Raiden">
+`;
 
