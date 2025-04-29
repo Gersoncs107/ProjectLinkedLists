f85c6ad9e07b3519e8393f2ebb535fff00eadef2
Gersoncs107 <gerson.silva107@gmail.com>
2025-04-28 23:29:53 -0300

    gitAdd

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 // webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -29,10 +26,10 @@ module.exports = {
         test: /\.html$/i,
         loader: "html-loader",
       },
-      // {
-      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      //   type: "asset/resource",
-      // },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
